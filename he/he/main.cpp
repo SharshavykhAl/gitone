3f78f088f7ce18e312b801e84927a1116a5f36de
AlexSharsh <sharshavykh.al@gmail.com>
2024-12-24 10:53:27 +0300

    Commit new vetka

---
+++ he/he/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  he
+//
+//  Created by Alexandr Sharshavykh on 24.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
